aa44f8bd78075e5edf413a7b52a25d0a8c9c3f93
Adrian Dimech <dimecha@amazon.com>
2024-04-04 16:05:31 +1100

    fix: update resynth strategy globs

--- packages/pdk-synth/src/pdk-synth.ts
+++ packages/pdk-synth/src/pdk-synth.ts
@@ -166,7 +166,7 @@ export class PDKSynth extends Component {
     );
 
     this.sourceRepository.setResynthStrategies([{
-      globs: ['.projen/*', '**/.projen/*', '**/.git*', '.git*', '.projenrc*', '**/projenrc*', 'package.json', '**/package.json', 'project.json', '**/project.json', 'pnpm-workspace.yaml', 'pnpm-lock.yaml', 'yarn.lock', 'package-lock.json', 'bun.lockb', 'poetry.lock', '**/poetry.lock'],
+      globs: ['.projen/*', '**/.projen/*', 'pnpm-lock.yaml', 'yarn.lock', 'package-lock.json', 'bun.lockb', 'poetry.lock', '**/poetry.lock'],
       identifier: `${projectName}-alwaysUpdate`,
       strategy: MergeStrategies.alwaysUpdate,
     }, {
