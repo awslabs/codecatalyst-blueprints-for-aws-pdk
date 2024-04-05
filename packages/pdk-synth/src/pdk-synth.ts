c653408a9c696bbd366083a357cb1ede2b3d93c7
Adrian Dimech <dimecha@amazon.com>
2024-04-05 16:38:45 +1100

    fix: resolve merge issues

--- packages/pdk-synth/src/pdk-synth.ts
+++ packages/pdk-synth/src/pdk-synth.ts
@@ -165,17 +165,13 @@ export class PDKSynth extends Component {
     );
 
     this.sourceRepository.setResynthStrategies([{
-      globs: ['.projen/*', '**/.projen/*', 'pnpm-lock.yaml', 'yarn.lock', 'package-lock.json', 'bun.lockb', 'poetry.lock', '**/poetry.lock'],
+      globs: ['.projen/*', '**/.projen/*', '**/.git*', '.git*', '.projenrc*', '**/projenrc*', 'package.json', '**/package.json', 'project.json', '**/project.json', 'pnpm-workspace.yaml', 'pnpm-lock.yaml', 'yarn.lock', 'package-lock.json', 'bun.lockb', 'poetry.lock', '**/poetry.lock'],
       identifier: `${projectName}-alwaysUpdate`,
       strategy: MergeStrategies.alwaysUpdate,
     }, {
       globs: ['README.md', '**/README.md'],
       identifier: `${projectName}-onlyAdd`,
       strategy: MergeStrategies.onlyAdd,
-    }, {
-      globs: ['**/.git*', '.git*', '.projenrc*', '**/projenrc*', 'package.json', '**/package.json', 'project.json', '**/project.json', 'pnpm-workspace.yaml'],
-      identifier: `${projectName}-preferProposed`,
-      strategy: MergeStrategies.preferProposed,
     }]);
   }
 
